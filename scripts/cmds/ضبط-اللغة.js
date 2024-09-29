0ee257cf11943a866f652fb63dd7d9479e90436d
gb-on <projectforbot@gmail.com>
2024-09-29 07:24:36 +0100

    Update ضبط-اللغة.js

--- scripts/cmds/ضبط-اللغة.js
+++ scripts/cmds/ضبط-اللغة.js
@@ -1,43 +1,25 @@
 const fs = require("fs-extra");
 
-module.exports = {
-	config: {
-		name: "setlang",
-		version: "1.5",
-		author: "NTKhang",
-		countDown: 5,
-		role: 0,
-		description: {
-			vi: "Cài đặt ngôn ngữ của bot cho nhóm chat hiện tại hoặc tất cả các nhóm chat",
-			en: "Set default language of bot for current chat or all chats"
-		},
-		category: "owner",
-		guide: {
-			vi: "   {pn} <language code ISO 639-1"
-				+ "\n   Ví dụ:"
-				+ "\n    {pn} en"
-				+ "\n    {pn} vi",
-			en: "\n   {pn} <language code ISO 639-1"
-				+ "\n   Example:"
-				+ "\n    {pn} en"
-				+ "\n    {pn} vi"
-		}
-	},
+module.exports = { config: {
+		      name: "ضبط-اللغة",
+	              aliases: ["ضبط5"],
+		      version: "1.5",
+		      author: "NTKhang", // تعريب: محمد تانجيرو \\
+		      countDown: 5,
+		      role: 0,
+		      description: { ar: "قم بتعيين اللغة الافتراضية للبوت في المجموعة الحالية أو في جميع المجموعات"},
+		      category: "owner",
+		      guide: { ar: " {pn} [رمز اللغة ISO 639-1]\n"
+				 + " Example:\n"
+				 + " {pn} en\n"
+				 + " {pn} ar\n"
+		           } },
 
-	langs: {
-		vi: {
-			setLangForAll: "Đã cài đặt ngôn ngữ mặc định cho bot là: %1",
-			setLangForCurrent: "Đã cài đặt ngôn ngữ mặc định cho nhóm chat này là: %1",
-			noPermission: "Chỉ admin bot mới có thể sử dụng lệnh này",
-			langNotFound: "Không tìm thấy ngôn ngữ: %1"
-		},
-		en: {
-			setLangForAll: "Set default language of bot to: %1",
-			setLangForCurrent: "Set default language for current chat: %1",
-			noPermission: "Only bot admin can use this command",
-			langNotFound: "Can't find language: %1"
-		}
-	},
+	langs: { ar: { setLangForAll: "تم تعيين اللغة الافتراضية للبوت إلى: %1",
+		       setLangForCurrent: "ضبط اللغة الافتراضية للمحموعة الحالية إلى: %1",
+		       noPermission: "يمكن للمطور فقط استخدام هذا الأمر",
+		       langNotFound: "لا يمكن العثور على لغة: %1"
+	       }     },
 
 	onStart: async function ({ message, args, getLang, threadsData, role, event }) {
 		if (!args[0])
