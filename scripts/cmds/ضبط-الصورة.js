aa332d4da4110d728c8fdbeda9e66f9c7fa0f811
gb-on <projectforbot@gmail.com>
2024-09-29 07:50:44 +0100

    Update ضبط-الصورة.js

---
+++ scripts/cmds/ضبط-الصورة.js
@@ -0,0 +1,54 @@
+const axios = require("axios");
+
+module.exports = { config: {
+		      name: "ضبط-الصورة",
+		      aliases: ["ضبط7"],
+		      version: "1.3",
+		      author: "NTKhang", // تعريب: محمد تانجيرو \\
+		      countDown: 5,
+		      role: 2,
+		      description: { ar: "تغيير صورة البوت"},
+		      category: "owner",
+		      guide: { ar: "   {pn} [<image url> | <phản hồi tin nhắn có ảnh>] [<caption> | để trống] [<expirationAfter (seconds)> | để trống]"
+				 + "\nPhản hồi 1 tin nhắn có chứa ảnh với nội dung: {pn}"
+				 + "\nGửi kèm 1 tin nhắn có chứa ảnh với nội dung: {pn}"
+				 + "\n\nGhi chú:"
+				 + "\n  + caption: caption sẽ đăng kèm khi đổi avatar"
+				 + "\n  + expirationAfter: đặt chế độ ảnh đại diện tạm thời (hết hạn sau expirationAfter(seconds))"
+				 + "\nVí dụ:"
+				 + "\n   {pn} https://example.com/image.jpg: (đổi ảnh đại diện không caption, không hết hạn)"
+				 + "\n   {pn} https://example.com/image.jpg Hello: (đổi ảnh đại diện với caption là \"Hello\", không hết hạn)"
+				 + "\n   {pn} https://example.com/image.jpg Hello 3600: (đổi ảnh đại diện với caption là \"Hello\", đặt tạm thời 1h)"
+		           } },
+
+	langs: { ar: { cannotGetImage: "❌ | حدث خطأ أثناء الاستعلام عن رابط للصورة",
+		       invalidImageFormat: "❌ | تنسيق الصورة غير صالح",
+		       changedAvatar: "✅ | تم تغيير صورة البوت بنجاح"
+	       }     },
+
+	onStart: async function ({ message, event, api, args, getLang }) {
+		const imageURL = (args[0] || "").startsWith("http") ? args.shift() : event.attachments[0]?.url || event.messageReply?.attachments[0]?.url;
+		const expirationAfter = !isNaN(args[args.length - 1]) ? args.pop() : null;
+		const caption = args.join(" ");
+		if (!imageURL)
+			return message.SyntaxError();
+		let response;
+		try {
+			response = (await axios.get(imageURL, {
+				responseType: "stream"
+			}));
+		}
+		catch (err) {
+			return message.reply(getLang("cannotGetImage"));
+		}
+		if (!response.headers["content-type"].includes("image"))
+			return message.reply(getLang("invalidImageFormat"));
+		response.data.path = "avatar.jpg";
+
+		api.changeAvatar(response.data, caption, expirationAfter ? expirationAfter * 1000 : null, (err) => {
+			if (err)
+				return message.err(err);
+			return message.reply(getLang("changedAvatar"));
+		});
+	}
+};
