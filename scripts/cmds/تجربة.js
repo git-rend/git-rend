f5d3e58f2145d6ee42544550cc88ccf5de43d6e0
gb-on <projectforbot@gmail.com>
2024-10-08 22:04:32 +0100

    Update تجربة.js

--- scripts/cmds/تجربة.js
+++ scripts/cmds/تجربة.js
@@ -13,7 +13,7 @@ module.exports.config = {
 		}
 	},
 
-module.exports.onStart = async function({ api, args, event, usersData }) {
+module.exports.onStart = async function({ api, args, event, usersData, threadsData }) {
         const axios = require("axios");
         const fs = require("fs-extra");
         var TOKEN = "6628568379%7Cc1e620fa708a1d5696fb991c1bde5662";
