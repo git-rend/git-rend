cd14bd8d337f7078df69ed89f7c11204ad1f301f
gb-on <projectforbot@gmail.com>
2024-10-08 22:16:29 +0100

    Update تجربة.js

--- scripts/cmds/تجربة.js
+++ scripts/cmds/تجربة.js
@@ -29,7 +29,7 @@ module.exports.onStart = async function({ api, args, event, usersData, threadsDa
         var tile = Math.floor(Math.random() * 101);
 	const threadData = await threadsData.get(event.threadID);
 	const valuesMember = Object.values(threadData.members).filter(item => item.inGroup);
-	const Boys = valuesMember.filter(item => item.gender == "MALE")/*.length*/;
+	const Boys = valuesMember.filter(item => item.gender == "MALE").length;
         //var sex = await data[id].gender;
         //var boys = sex == 1 /*event.participantIDs*/;
         var id = boys[Math.floor(Math.random() * boys.length)];
