113a978e2ee5cdb2b0aeff04868d87d5b2a7b7b7
gb-on <projectforbot@gmail.com>
2024-10-13 11:00:22 +0100

    Update أصدقاء.js

--- scripts/cmds/أصدقاء.js
+++ scripts/cmds/أصدقاء.js
@@ -14,13 +14,12 @@ const fs = require('fs');
 const axios = require('axios');
 const { loadImage, createCanvas } = require('canvas');
 
-module.exports.onStart = async function ({ api, event, args, circle }) {
+module.exports.onStart = async function ({ api, event, args }) {
   let { senderID, threadID, messageID } = event;
   let pathImg = __dirname + "/images/married.png";
-  let /*pathAva*/pathprof1 = __dirname + "/images/profile1.png";
-/*1*/let pathprof2 = __dirname + "/images/profile2.png";
-/*2*/var Member1 = senderID;
-  //if (!args[0]) {/*var uid = senderID*/return api.sendMessage("تاغ لشخص أو رد على رسالته",threadID,messageID)};
+  let pathprof1 = __dirname + "/images/profile1.png";
+  let pathprof2 = __dirname + "/images/profile2.png";
+  var Member1 = senderID;
   if (event.type == "message_reply") { var Member2 = event.messageReply.senderID }
   if (args.join().indexOf('@') !== -1) { var Member2 = Object.keys(event.mentions) } 
   if (!args[0]) { return api.sendMessage("تاغ لشخص أو رد على رسالته",threadID,messageID)}
@@ -32,8 +31,8 @@ module.exports.onStart = async function ({ api, event, args, circle }) {
   let getimage = ( await axios.get(`https://i.imgur.com/hmKmmam.jpg`, { responseType: "arraybuffer", })).data;
   fs.writeFileSync(pathImg, Buffer.from(getimage, "utf-8"));
   let baseImage = await loadImage(pathImg);
-  let baseprof1 = await /*loadImage*/circle(pathprof1);
-  let baseprof2 = await /*loadImage*/circle(pathprof2);
+  let baseprof1 = await loadImage(pathprof1);
+  let baseprof2 = await loadImage(pathprof2);
   let canvas = createCanvas(baseImage.width, baseImage.height);
   let ctx = canvas.getContext("2d");
   ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
