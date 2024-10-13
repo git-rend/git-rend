d9a2f4ff75dae6ee347914dd3ea6fc3808ceb147
gb-on <projectforbot@gmail.com>
2024-10-13 06:40:33 +0100

    Update تزوجت.js

--- scripts/cmds/تزوجت.js
+++ scripts/cmds/تزوجت.js
@@ -22,7 +22,7 @@ module.exports = { config: {
 
 		circle: async function (image) {
 				const jimp = require("jimp");
-				//image = await jimp.read(image);
+				const image = await jimp.read(image);
 				image.circle();
 				return await image.getBufferAsync("image/png");
 		},
