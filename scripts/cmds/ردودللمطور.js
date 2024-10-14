3487dd3e43cb43f0e6402cb842465f21e08a4596
gb-on <projectforbot@gmail.com>
2024-10-14 12:31:39 +0100

    Create ردودللمطور.js

---
+++ scripts/cmds/ردودللمطور.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+module.exports.config = {
+                  name: "ردودللمطور",
+            		  version: "1.3",
+            		  author: "محمد تانجيرو",
+            		  countDown: 5,
+            		  role: 0,
+            		  description: { ar: "ردود البوت على بعض الرسائل دون الحاجة إلى بادئة" },
+            		  category: "no prefix",
+            		  guide: { ar: "لا تحتاج للبادئة (.)" }
+                        };
+module.exports.onStart = function({ message, api, event, client, envGlobal, __GLOBAL }) { }
+module.exports.onChat = async function({ message, api, event, client, envGlobal, __GLOBAL }) {
+  var { threadID, messageID, reason, senderID } = event;
+  let mhmd = event.body;
+
+if (mhmd.includes ("هلاوات")) {
+    return api.sendMessage("أحلى سلام من أحلى مطوࢪ 🤭\nنوࢪت الڪل محمد تانجيࢪو 👋",
+  event.threadID,event.messageID)};
+  
+};
