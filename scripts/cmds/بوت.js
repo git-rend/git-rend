9e97b3558d52d286160fbff2e68140b72b0e26d2
gb-on <projectforbot@gmail.com>
2024-10-15 17:25:34 +0100

    Create بوت.js

---
+++ scripts/cmds/بوت.js
@@ -0,0 +1,13 @@
+module.exports.config = {
+                  name: "بوت",
+                  version:"1.0.0", 
+                  role: 2,
+                  countdown: 5,
+                  author: "محمد تانجيرو", 
+                  description:{ ar : ""},
+                  category: "تجارب", 
+                  guide: { ar : "{pn}"}
+                        };
+module.exports.onStart = async fanction ( api ) {
+return api.sendMessage ("بوت أكاني",event.threadID, event.messageID)
+  }
