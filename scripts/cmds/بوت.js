fc84b09d9467de6aa778bad93a88ee6e6a49aa90
gb-on <projectforbot@gmail.com>
2024-10-16 09:13:24 +0100

    Update بوت.js

--- scripts/cmds/بوت.js
+++ scripts/cmds/بوت.js
@@ -9,189 +9,88 @@ module.exports.config = {
                   guide: { ar : "{pn}"}
                         };
 module.exports.onStart = async function ({ api, event }) {
-  const first = `██████╗ 
+const first = `
+██████╗░
 ██╔══██╗
-██████╔╝
+██████╦╝
 ██╔══██╗
-██████╔╝
-╚═════╝`;
-  const second = `|  ██████╗ 
-██╔═══██╗
-██║         ██║
-██║         ██║
-╚██████╔╝
-   ╚═════╝`;
-  const third = `████████╗
+██████╦╝
+╚═════╝░`;
+const second = `
+░█████╗░
+██╔══██╗
+██║░░██║
+██║░░██║
+╚█████╔╝
+░╚════╝░`;
+const third = `
+████████╗
 ╚══██╔══╝
-         ██║   
-         ██║   
-         ██║   
-         ╚═╝`;
-  { const Message = await api.sendMessage(first, event.threadID/*, event.messageID*/);
+░░░██║░░░
+░░░██║░░░
+░░░██║░░░
+░░░╚═╝░░░`;
+const fourd = `
+░█████╗░
+██╔══██╗
+███████║
+██╔══██║
+██║░░██║
+╚═╝░░╚═╝`; 
+const fifth = `
+██╗░░██╗
+██║░██╔╝
+█████═╝░
+██╔═██╗░
+██║░╚██╗
+╚═╝░░╚═╝`; 
+const sixth = `
+███╗░░██╗
+████╗░██║
+██╔██╗██║
+██║╚████║
+██║░╚███║
+╚═╝░░╚══╝`;
+const seventh = `
+███████╗
+██╔════╝
+█████╗░░
+██╔══╝░░
+███████╗
+╚══════╝`;
+  { const Message1 = await api.sendMessage(first, event.threadID/*, event.messageID*/);
    
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
+                     await new Promise((resolve) => setTimeout(resolve, 3000));
+                     await api.editMessage( second, Message.messageID, event.threadID);
 
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
+                     await new Promise((resolve) => setTimeout(resolve, 3000));
+                     await api.editMessage( third, Message.messageID, event.threadID);
 
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID);
+                     await new Promise((resolve) => setTimeout(resolve, 3000));
+                     await api.editMessage( first, Message.messageID, event.threadID);
    
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
+                     await new Promise((resolve) => setTimeout(resolve, 3000));
+                     await api.editMessage( second, Message.messageID, event.threadID);
 
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
+                     await new Promise((resolve) => setTimeout(resolve, 3000));
+                     await api.editMessage( first + "\n" + second + "\n" + third, Message.messageID, event.threadID)
+  };
 
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID);
+  { const Message2 = await api.sendMessage(fourd, event.threadID/*, event.messageID*/);
+
+                     await new Promise((resolve) => setTimeout(resolve, 3000));
+                     await api.editMessage( fifth, Message.messageID, event.threadID);
                       
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
+                     await new Promise((resolve) => setTimeout(resolve, 3000));
+                     await api.editMessage( fourd, Message.messageID, event.threadID);
 
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
+                     await new Promise((resolve) => setTimeout(resolve, 3000));
+                     await api.editMessage( sixth, Message.messageID, event.threadID);
 
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID);
+                     await new Promise((resolve) => setTimeout(resolve, 3000));
+                     await api.editMessage( seventh, Message.messageID, event.threadID);
   
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
+                     await new Promise((resolve) => setTimeout(resolve, 3000));
+                     await api.editMessage( fourd + "\n" + fifth + "\n" + fourd + "\n" + sixth + "\n" + seventh, Message.messageID, event.threadID);
 
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID);
-  
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID);
-  
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID);
-  
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID);
-  
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID);
-  
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID);  
-  
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID);  
-  
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID); 
-  
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID);  
-  
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID); 
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID);  
-  
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID); 
-  
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( first, Message.messageID, event.threadID);  
-  
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( second, Message.messageID, event.threadID);
-
-                    await new Promise((resolve) => setTimeout(resolve, 3000));
-                    await api.editMessage( third, Message.messageID, event.threadID);
-  
-  
-  
-  
-  
-  
-  
-  }}
+  }                                                      }
