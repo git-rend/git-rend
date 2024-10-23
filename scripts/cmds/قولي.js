915e5ccef8061b4292a194f1cd821ea7b6585b19
akane-B <akanekinoshita2024@gmail.com>
2024-10-23 10:34:50 +0100

    Update قولي.js

--- scripts/cmds/قولي.js
+++ scripts/cmds/قولي.js
@@ -15,7 +15,7 @@ module.exports.onStart = async function({ api, event, args }) {
 		const { createReadStream, unlinkSync } = require ("fs-extra");
 		const { resolve } = require ("path");
 		var content = (event.type == "message_reply") ? event.messageReply.body : args.join(" ");
-		var languageToSay = (["ru","en","ko","ja","ar"].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.config.language;
+		var languageToSay = (["ru","en","ko","ja","ar"].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.GoatBot.config.language;
 		var msg = (languageToSay != global.config.language) ? content.slice(3, content.length) : content;
 		const path = resolve(__dirname, 'cache', `${event.threadID}_${event.senderID}.mp3`);
 		await global.utils.downloadFile(`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(msg)}&tl=ar&client=tw-ob`, path);
