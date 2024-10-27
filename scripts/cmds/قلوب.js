054300ee5a8c2661e191af25d04879e6e488e453
unknown-pe <akanebot2005@gmail.com>
2024-10-27 17:36:33 +0100

    Update قلوب.js

--- scripts/cmds/قلوب.js
+++ scripts/cmds/قلوب.js
@@ -10,8 +10,8 @@ module.exports.config = {
 	          guide: { ar: "وتدخل القلب أو اللون الذي اخترته"},
                         };
 module.exports.onStart = async function ({event, api, args, usersData, __GLOBAL }) {
-   var userData = await usersData.get(event.senderID);
-   var money = userData.money
+   var data = await usersData.get(event.senderID);
+   var money = data.money
    //const moneyUser = (await Currencies.getData(event.senderID)).money;
    if (200 > money) return api.sendMessage("تحتاج  200$ للعب لعبة: قلوب 🙄", event.threadID, event.messageID);
    var color = args.join(" ") 
@@ -25,11 +25,11 @@ module.exports.onStart = async function ({event, api, args, usersData, __GLOBAL 
    else if (color == "ابيض" || color == "🤍" || color == "white") color = 5;
    else return api.sendMessage("✨ الاختيار يجب أن يكون: ✨\n        ا—-—-—-—-—-—ا\n🧡 / برتقالي / orange\n💜 / بنفسجي / violet\n💛 / اصفر / yellow\n💚 / اخضر / green\n🤍 / ابيض / white\n💙 / ازرق/ blue\n\n  ✨اختر اللون المناسب: ✨\n    سواء بالقلوب أو بالكتابة", event.threadID, event.messageID);
    let coin = Math.floor(Math.random() * (1000 - 500 + 1));
-   if (color == 0 && check(random) == '💙') api.sendMessage(`✨ —- مبارك عليك 😍 -— ✨\nاختيارك موافق لاختياري: (💙)\n\n  خذ: ${coin} $ جائزة لفوزك 🤭`, event.threadID, event.messageID);
-   else if (color == 1 && check(random) == '🧡') api.sendMessage(`✨ —- مبارك عليك 😍 -— ✨\nاختيارك موافق لاختياري: (🧡)\n\n  خذ: ${coin} $ جائزة لفوزك 🤭`, event.threadID, event.messageID);
-   else if (color == 2 && check(random) == '💚') api.sendMessage(`✨ —- مبارك عليك 😍 -— ✨\nاختيارك موافق لاختياري: (💚)\n\n  خذ: ${coin} $ جائزة لفوزك 🤭`, event.threadID, event.messageID);
-   else if (color == 3 && check(random) == '💛') api.sendMessage(`✨ —- مبارك عليك 😍 -— ✨\nاختيارك موافق لاختياري: (💛)\n\n  خذ: ${coin} $ جائزة لفوزك 🤭`, event.threadID, event.messageID);
-   else if (color == 4 && check(random) == '💜') api.sendMessage(`✨ —- مبارك عليك 😍 -— ✨\nاختيارك موافق لاختياري: (💜)\n\n  خذ: ${coin} $ جائزة لفوزك 🤭`, event.threadID, event.messageID);
-   else if (color == 5 && check(random) == '🤍') api.sendMessage(`✨ —- مبارك عليك 😍 -— ✨\nاختيارك موافق لاختياري: (🤍)\n\n  خذ: ${coin} $ جائزة لفوزك 🤭`, event.threadID,/*() => await usersData.set(senderID, {money: userData.money + coin, data: userData.data}*/,event.messageID);
-   else api.sendMessage(`خطأ 🤣، اختياري هو: (${check(random)})\nحاول مجددا، وأتمنى تفوز 🥹\n            -----------------\n             - 1000 $`, event.threadID, event.messageID);
-	}
+   if (color == 0 && check(random) == '💙') api.sendMessage(`✨ —- مبارك عليك 😍 -— ✨\nاختيارك موافق لاختياري: (💙)\n\n  خذ: ${coin} $ جائزة لفوزك 🤭`, event.threadID,() => Currencies.setData(event.senderID, options = {money: money + coin}),event.messageID);
+   else if (color == 1 && check(random) == '🧡') api.sendMessage(`✨ —- مبارك عليك 😍 -— ✨\nاختيارك موافق لاختياري: (🧡)\n\n  خذ: ${coin} $ جائزة لفوزك 🤭`, event.threadID,() => Currencies.setData(event.senderID, options = {money: money + coin}),event.messageID);
+   else if (color == 2 && check(random) == '💚') api.sendMessage(`✨ —- مبارك عليك 😍 -— ✨\nاختيارك موافق لاختياري: (💚)\n\n  خذ: ${coin} $ جائزة لفوزك 🤭`, event.threadID,() => Currencies.setData(event.senderID, options = {money: money + coin}),event.messageID);
+   else if (color == 3 && check(random) == '💛') api.sendMessage(`✨ —- مبارك عليك 😍 -— ✨\nاختيارك موافق لاختياري: (💛)\n\n  خذ: ${coin} $ جائزة لفوزك 🤭`, event.threadID,() => Currencies.setData(event.senderID, options = {money: money + coin}),event.messageID);
+   else if (color == 4 && check(random) == '💜') api.sendMessage(`✨ —- مبارك عليك 😍 -— ✨\nاختيارك موافق لاختياري: (💜)\n\n  خذ: ${coin} $ جائزة لفوزك 🤭`, event.threadID,() => Currencies.setData(event.senderID, options = {money: money + coin}),event.messageID);
+   else if (color == 5 && check(random) == '🤍') api.sendMessage(`✨ —- مبارك عليك 😍 -— ✨\nاختيارك موافق لاختياري: (🤍)\n\n  خذ: ${coin} $ جائزة لفوزك 🤭`, event.threadID,() => Currencies.setData(event.senderID, options = {money: money + coin}),event.messageID);
+   else api.sendMessage(`خطأ 🤣، اختياري هو: (${check(random)})\nحاول مجددا، وأتمنى تفوز 🥹\n            -----------------\n             - 1000 $`, event.threadID,() => Currencies.setData(event.senderID, options = {money: money - 1000}),event.messageID);
+       }
