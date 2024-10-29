bd168fe4229ebd6e01a0804762508e82280e7eca
git-rend <githubrender1@gmail.com>
2024-10-29 22:12:01 +0100

    Update بوت.js

--- scripts/cmds/بوت.js
+++ scripts/cmds/بوت.js
@@ -8,89 +8,92 @@ module.exports.config = {
                   category: "تجارب", 
                   guide: { ar : "{pn}"}
                         };
+
 module.exports.onStart = async function ({ api, event }) {
-const first = `
+
+const B = `
 ██████╗░
 ██╔══██╗
 ██████╦╝
 ██╔══██╗
 ██████╦╝
 ╚═════╝░`;
-const second = `
+const O = `
 ░█████╗░
 ██╔══██╗
 ██║░░██║
 ██║░░██║
 ╚█████╔╝
 ░╚════╝░`;
-const third = `
+const T = `
 ████████╗
 ╚══██╔══╝
 ░░░██║░░░
 ░░░██║░░░
 ░░░██║░░░
 ░░░╚═╝░░░`;
-const fourd = `
+const A = `
 ░█████╗░
 ██╔══██╗
 ███████║
 ██╔══██║
 ██║░░██║
 ╚═╝░░╚═╝`; 
-const fifth = `
+const K = `
 ██╗░░██╗
 ██║░██╔╝
 █████═╝░
 ██╔═██╗░
 ██║░╚██╗
 ╚═╝░░╚═╝`; 
-const sixth = `
+const N = `
 ███╗░░██╗
 ████╗░██║
 ██╔██╗██║
 ██║╚████║
 ██║░╚███║
 ╚═╝░░╚══╝`;
-const seventh = `
+const E = `
 ███████╗
 ██╔════╝
 █████╗░░
 ██╔══╝░░
 ███████╗
 ╚══════╝`;
-  { const Message1 = await api.sendMessage(first, event.threadID/*, event.messageID*/);
+  
+  { const Message1 = await api.sendMessage( B, event.threadID/*, event.messageID*/);
    
                      await new Promise((resolve) => setTimeout(resolve, 3000));
-                     await api.editMessage( second, Message1.messageID, event.threadID);
+                     await api.editMessage( O, Message1.messageID, event.threadID);
 
                      await new Promise((resolve) => setTimeout(resolve, 3000));
-                     await api.editMessage( third, Message1.messageID, event.threadID);
+                     await api.editMessage( T, Message1.messageID, event.threadID);
 
                      await new Promise((resolve) => setTimeout(resolve, 3000));
-                     await api.editMessage( first, Message1.messageID, event.threadID);
+                     await api.editMessage( B, Message1.messageID, event.threadID);
    
                      await new Promise((resolve) => setTimeout(resolve, 3000));
-                     await api.editMessage( second, Message1.messageID, event.threadID);
+                     await api.editMessage( O, Message1.messageID, event.threadID);
 
                      await new Promise((resolve) => setTimeout(resolve, 3000));
-                     await api.editMessage( first + "\n" + second + "\n" + third, Message1.messageID, event.threadID)
+                     await api.editMessage( B + "\n" + O + "\n" + T, Message1.messageID, event.threadID)
   };
 
-  { const Message2 = await api.sendMessage(fourd, event.threadID/*, event.messageID*/);
+  { const Message2 = await api.sendMessage( A, event.threadID/*, event.messageID*/);
 
                      await new Promise((resolve) => setTimeout(resolve, 3000));
-                     await api.editMessage( fifth, Message2.messageID, event.threadID);
+                     await api.editMessage( K, Message2.messageID, event.threadID);
                       
                      await new Promise((resolve) => setTimeout(resolve, 3000));
-                     await api.editMessage( fourd, Message2.messageID, event.threadID);
+                     await api.editMessage( A, Message2.messageID, event.threadID);
 
                      await new Promise((resolve) => setTimeout(resolve, 3000));
-                     await api.editMessage( sixth, Message2.messageID, event.threadID);
+                     await api.editMessage( N, Message2.messageID, event.threadID);
 
                      await new Promise((resolve) => setTimeout(resolve, 3000));
-                     await api.editMessage( seventh, Message2.messageID, event.threadID);
+                     await api.editMessage( E, Message2.messageID, event.threadID);
   
                      await new Promise((resolve) => setTimeout(resolve, 3000));
-                     await api.editMessage( fourd + "\n" + fifth + "\n" + fourd + "\n" + sixth + "\n" + seventh, Message2.messageID, event.threadID);
+                     await api.editMessage( A + "\n" + K + "\n" + A + "\n" + N + "\n" + E, Message2.messageID, event.threadID);
 
   }                                                      }
