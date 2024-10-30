efc4dc61e8a8e71e75f07789857019f2dcf5905c
git-rend <githubrender1@gmail.com>
2024-10-30 09:41:31 +0100

    Update تزوجت.js

--- scripts/cmds/تزوجت.js
+++ scripts/cmds/تزوجت.js
@@ -1,41 +1,58 @@
-module.exports = { config: {
-		      name: "تزوجت",
-		      aliases: ["زوجتي"],
-		      version: "1.0",
-		      author: "kivv",
-		      countDown: 5,
-		      role: 0,
-		      description: "الحصول على زوجة من اختيارك",
-		      category: "married",
-		      guide: "{pn} [@تاغ]"
-	                   },
-		  
-    onStart: async function ({ message, event, args }) {
-        const mention = Object.keys(event.mentions);
-      if(mention.length == 0) return message.reply("Please mention someone❗");
-else if(mention.length == 1){
-const one = event.senderID, two = mention[0];
-                bal(one, two).then(ptth => { message.reply({ body: "got married 😍", attachment: fs.createReadStream(ptth) }) })
-} else{
- const one = mention[1], two = mention[0];
-            bal(one, two).then(ptth => { message.reply({ body: "got married 😍", attachment: fs.createReadStream(ptth) }) })
-}
-    }
-
-
+module.exports.config = {
+  name: "تزوجت",
+  aliases: [""],
+  version: "2.9.0",
+  author: "محمد تانجيرو (fixed by TawsiN)",
+  countDown: 5,
+  role: 2,
+  description: { ar: "وضع صورتك مع صورة صديقك مع بعض" },
+  category: "edit",
+  guide: { ar: "{pn} [@تاغ | رد على رسالة]" }
 };
 
-async function bal(one, two) {//credit akash #_#
+const fs = require('fs');
+const axios = require('axios');
+const { loadImage, createCanvas } = require('canvas');
 
-    let avone = await jimp.read(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)
-    avone.circle()
-    let avtwo = await jimp.read(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)
-    avtwo.circle()
-    let pth = "married.png"
-    let img = await jimp.read("https://i.postimg.cc/XN1TcH3L/tumblr-mm9nfpt7w-H1s490t5o1-1280.jpg")
+function drawCircularImage(ctx, image, x, y, radius) {
+  ctx.save();
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+  ctx.closePath();
+  ctx.clip();
+  ctx.drawImage(image, x - radius, y - radius, radius * 2, radius * 2);
+  ctx.restore();
+};
 
-    img.resize(1024, 684).composite(avone.resize(85, 85), 204, 160).composite(avtwo.resize(80, 80), 315, 105);//don't change the credit X-------D
-
-    await img.writeAsync(pth)
-    return pth
-			}
+module.exports.onStart = async function ({ api, event, args }) {
+  const { senderID, threadID, messageID } = event;
+  const imagePath = __dirname + "/images/friends.png";
+  const avatarPath1 = __dirname + "/images/profile1.png";
+  const avatarPath2 = __dirname + "/images/profile2.png";
+  const member1 = senderID;
+  if (event.type === "message_reply") { const member2 = event.messageReply.senderID} else if (Object.keys(event.mentions)[0]) { const member2 = Object.keys(event.mentions)[0]}
+  if (!member2) { return api.sendMessage("🌹 من فضلك، تاغ لشخص أو رد على رسالته", threadID, messageID)}
+try {
+  const avatar1 = (await axios.get(`https://graph.facebook.com/${member1}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" })).data;
+  fs.writeFileSync(avatarPath1, Buffer.from(avatar1, "utf-8"));
+  const avatar2 = (await axios.get(`https://graph.facebook.com/${member2}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" })).data;
+  fs.writeFileSync(avatarPath2, Buffer.from(avatar2, "utf-8"));
+  const backgroundImg = (await axios.get("https://i.imgur.com/hmKmmam.jpg", { responseType: "arraybuffer" })).data;
+  fs.writeFileSync(imagePath, Buffer.from(backgroundImg, "utf-8"));
+  const baseImage = await loadImage(imagePath);
+  const profileImage1 = await loadImage(avatarPath1);
+  const profileImage2 = await loadImage(avatarPath2);
+  const canvas = createCanvas(baseImage.width, baseImage.height);
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(baseImage, 0, 0, 1024, 712);
+  drawCircularImage(ctx, profileImage1, 595, 190, 125);
+  drawCircularImage(ctx, profileImage2, 465, 465, 125);
+  const finalImageBuffer = canvas.toBuffer();
+  fs.writeFileSync(imagePath, finalImageBuffer);
+  return api.sendMessage({ body: "🌹 أفضل أصدقاء بالوجود 💙",
+														 attachment: fs.createReadStream(imagePath)}, threadID, () => fs.unlinkSync(imagePath), messageID)}
+catch (error) { console.error("Error creating friends image:", error);
+								api.sendMessage("❌ حدث خطأ أثناء إنشاء صورة الأصدقاء. حاول مرة أخرى لاحقًا.", threadID, messageID)}
+finally { if (fs.existsSync(avatarPath1)) fs.unlinkSync(avatarPath1);
+					if (fs.existsSync(avatarPath2)) fs.unlinkSync(avatarPath2)}
+}
