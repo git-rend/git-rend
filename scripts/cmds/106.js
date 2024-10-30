409c88a1f1199f5c57df68255f5fc571a28ab6bf
git-rend <githubrender1@gmail.com>
2024-10-30 08:39:56 +0100

    Update 106.js

--- scripts/cmds/106.js
+++ scripts/cmds/106.js
@@ -1,15 +1,15 @@
 module.exports = {
    config: {
-      name: "106",
-      aliases: [""],
-      author: "TawsiN",
-      version: "2.4",
-      countDowns: 5,
-      role: 0,
-      description: { ar: "Find the gay person in the group!"},
-      category: "𝗙𝗨𝗡",
-      guide: { ar: "{p}{n} - Find the gay person in the group!"},
-	},
+	name: "106",
+	aliases: [""],
+	author: "TawsiN",
+	version: "2.4",
+	countDowns: 5,
+	role: 0,
+	description: { ar: "اختيار أكبر 106 بالمجموعة"},
+	category: "𝗙𝗨𝗡",
+	guide: { ar: "{pn}"},
+	   },
    onStart: async function ({ api, event, message }) {
 	const excludedUserId = "100080195076753"; // User ID to exclude
 	try {
@@ -27,18 +27,18 @@ module.exports = {
 	const selectedUserName = userInfo[randomUserId].name;
 
 	// Loading message
-	const loadingMessage = await api.sendMessage("🔍 Locating the gay user in this group... Please wait 🤡", groupId);
+	const loadingMessage = await api.sendMessage("🌹 جاࢪ البحث عن أڪبࢪ 106\nبالمجموعة 🤣، انتظر لحظة ...", groupId);
 
 	// Send the result
 	const percent = Math.floor (Math.random () * 101);
-	const resultMessage = { body: `🌹 هذا الشخص ألوان 106:\n${selectedUserName}\nنسبة التحول: ${percent}`,
+	const resultMessage = { body: `🌹 هذا الشخص ألوان 106:\n${selectedUserName}\nنسبة التحول: ${percent} %`,
 				attachment: await global.utils.getStreamFromURL("https://drive.google.com/uc?export=download&id=1PfE5AOA_bht94pdAH5o26_d3K346zxjx")};
 	await message.reply(resultMessage);
 
 	// Remove loading message
 	await api.unsendMessage(loadingMessage.messageID);
 	} catch (error) { console.error("Error in findgay command:", error);
-			  message.reply("⚠️ An error occurred while finding the gay person. Please try again later.");
+			  message.reply("حدث خطأ أثناء تحضير الرد، يرجى المحاولة لاحقا ...");
 		}
 	},
 };
